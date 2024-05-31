bd17e6fbe00355d56b735c8b2af9659e7a12e975
nao109 <haruki.2004.109@gmail.com>
2024-05-31 21:02:17 +0900

    Replaced namespaces with classes for better encapsulation in Assembler

--- projects/6/assembler/Assembler.hpp
+++ projects/6/assembler/Assembler.hpp
@@ -1,7 +1,10 @@
 #ifndef ASSEMBLER_H_
 #define ASSEMBLER_H_
 
-namespace Assembler {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Assembler {
     struct ParseElement {
         string commandType;
         int address;
@@ -10,10 +13,158 @@ namespace Assembler {
         string comp;
         string jump;
     };
-}
 
-#include "SymbolTable.hpp"
-#include "Parser.hpp"
-#include "Code.hpp"
+    class Parser {
+        // 今読んでいるコードのアドレス
+        int address = 0;
+
+    public:
+        void advance();
+
+        int getAddress();
+
+        string commandType(string str);
+
+        string symbol(string str);
+
+        string dest(string str);
+
+        string comp(string str);
+
+        string jump(string str);
+    };
+
+    class Code {
+    public:
+        string dest(string dst);
+
+        string comp(string cmp);
+
+        string jump(string jmp);
+    };
+
+    struct SymbolTable {
+        map<string, int> symboltable;
+        int next_address;
+
+    public:
+        SymbolTable();
+        
+        int getNextAddress();
+
+        void incNextAddress();
+
+        void addEntry(string symbol, int address);
+
+        bool contains(string symbol);
+
+        int getAddress(string symbol);
+    };
+
+    vector<string> inputFile(string input_file){
+        // アセンブリの入力
+
+        // ifstreamで読み込む
+        ifstream ifs(input_file);
+
+        string str;
+        vector<string> s; // アセンブリのコード
+        while(getline(ifs, str)){
+            // 空行は無視
+            if(str[1] == '\0') continue;
+
+            // 空白は無視
+            int slash = 0;
+            string sstr;
+            for(char i : str){
+                if(i == ' ') continue;
+
+                if(i == '/'){
+                    if(slash == 0) slash++;
+                    else break;
+                }
+                else if(slash == 1){
+                    sstr.push_back('/');
+                    slash = 0;
+                }
+                if(slash == 0) sstr.push_back(i);
+            }
+            if(sstr.empty()) continue;
+            s.push_back(sstr);
+        }
+        return s;
+    }
+
+    void outputFile(string output_file, vector<string> decode){
+        // hackコードの出力
+        ofstream file(output_file);
+        for(string str : decode) file << str << "\n";
+    }
+
+    public:
+    void assemble(string input_file, string output_file){
+        vector<string> asm_code = inputFile(input_file); // アセンブリのコード
+
+        // Parser
+        Parser parser;
+        // Coder
+        Code code;
+        // シンボルとアドレスの対応表
+        SymbolTable symboltable;
+        for(string sstr : asm_code){
+            string commandType = parser.commandType(sstr);
+            string symbol = parser.symbol(sstr);
+
+            // L_COMMANDなら対応表に追加
+            if(commandType == "L_COMMAND"){
+                if(!symboltable.contains(symbol)){
+                    symboltable.addEntry(symbol, parser.getAddress());
+                }
+            }
+            // それ以外はアドレスをインクリメント
+            else parser.advance();
+        }
+
+        // hackコード
+        vector<string> decode;
+        for(string str : asm_code){
+            ParseElement e;
+            e.commandType = parser.commandType(str);
+            if(e.commandType == "A_COMMAND"){
+                e.symbol = parser.symbol(str);
+            }
+            else if(e.commandType == "L_COMMAND"){
+                e.symbol = parser.symbol(str);
+            }
+            else if(e.commandType == "C_COMMAND"){
+                e.dest = parser.dest(str);
+                e.comp = parser.comp(str);
+                e.jump = parser.jump(str);
+            }
+
+            if(e.commandType == "C_COMMAND"){
+                decode.push_back("111" + code.comp(e.comp) + code.dest(e.dest) + code.jump(e.jump));
+            }
+            else if(e.commandType == "A_COMMAND"){
+                // シンボルがアドレスでない場合
+                if(isalpha(e.symbol[0])){
+                    if(symboltable.contains(e.symbol)) e.address = symboltable.getAddress(e.symbol);
+                    else{
+                        // シンボルとアドレスを対応付け
+                        symboltable.addEntry(e.symbol, symboltable.getNextAddress());
+                        e.address = symboltable.getAddress(e.symbol);
+                        symboltable.incNextAddress();
+                    }
+                }
+                // シンボルがアドレスである場合
+                else e.address = stoi(e.symbol);
+                bitset<15> bs(e.address);
+                decode.push_back("0" + bs.to_string());
+            }
+        }
+
+        outputFile(output_file, decode);
+    }
+};
 
 #endif  // ASSEMBLER_H_
---
+++ projects/6/assembler/SymbolTable.cpp
@@ -0,0 +1,54 @@
+#include "Assembler.hpp"
+
+map<string, int> symbols = {
+    {"SP", 0},
+    {"LCL", 1},
+    {"ARG", 2},
+    {"THIS", 3},
+    {"THAT", 4},
+    {"R0", 0},
+    {"R1", 1},
+    {"R2", 2},
+    {"R3", 3},
+    {"R4", 4},
+    {"R5", 5},
+    {"R6", 6},
+    {"R7", 7},
+    {"R8", 8},
+    {"R9", 9},
+    {"R10", 10},
+    {"R11", 11},
+    {"R12", 12},
+    {"R13", 13},
+    {"R14", 14},
+    {"R15", 15},
+    {"SCREEN", 16384},
+    {"KEYBOARD", 24576}
+};
+
+Assembler::SymbolTable::SymbolTable(){
+    symboltable = symbols;
+
+    // RAM[16]から追加
+    next_address = 16;
+}
+        
+int Assembler::SymbolTable::getNextAddress(){
+    return next_address;
+}
+
+void Assembler::SymbolTable::incNextAddress(){
+    next_address++;
+}
+
+void Assembler::SymbolTable::addEntry(string symbol, int address){
+    symboltable[symbol] = address;
+}
+
+bool Assembler::SymbolTable::contains(string symbol){
+    return symboltable.count(symbol);
+}
+
+int Assembler::SymbolTable::getAddress(string symbol){
+    return symboltable[symbol];
+}
