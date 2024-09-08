70abfdfce2c0f62b72a123a478bf0cd4db78e96e
nao109 <haruki.2004.109@gmail.com>
2024-09-09 01:47:02 +0900

    Fix function kind2string()

--- projects/jackcompiler/Utils.cpp
+++ projects/jackcompiler/Utils.cpp
@@ -4,17 +4,13 @@ std::string kind2string(Kind kind){
     switch(kind){
         case Kind::STATIC:
             return "static";
-            break;
         case Kind::FIELD:
             return "field";
         case Kind::ARG:
             return "argument";
-            break;
         case Kind::VAR:
             return "var";
-            break;
         default:
             return "none";
-            break;
     }
 }
